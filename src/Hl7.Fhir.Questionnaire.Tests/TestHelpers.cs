b1be379b26dc96474f5a361056ab98bcb0aa3805
Brian Postlethwaite <brian_pos@hotmail.com>
2017-09-09 09:56:19 -0700

    initial port of the Questionnaire Structure Item changes (not building yet)

---
+++ src/Hl7.Fhir.Questionnaire.Tests/TestHelpers.cs
@@ -0,0 +1,64 @@
+﻿/* 
+ * Copyright (c) 2014, Furore (info@furore.com) and contributors
+ * See the file CONTRIBUTORS for details.
+ * 
+ * This file is licensed under the BSD 3-Clause license
+ * available at https://raw.githubusercontent.com/ewoutkramer/fhir-net-api/master/LICENSE
+ */
+
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hl7.Fhir.Model;
+using System.Xml.Linq;
+using System.ComponentModel.DataAnnotations;
+using Hl7.Fhir.Validation;
+using Hl7.Fhir.QuestionnaireServices;
+using Hl7.Fhir.Specification.Source;
+using Hl7.Fhir.Rest;
+using Hl7.Fhir.FhirPath;
+
+namespace Hl7.Fhir.QuestionnaireServices.Tests
+{
+    [TestClass]
+    public static class TestHelpers
+    {
+        [AssemblyInitialize]
+        public static void SetupSource(TestContext context)
+        {
+            // Ensure the FHIR extensions are registered
+            ElementNavFhirExtensions.PrepareFhirSymbolTableFunctions();
+
+            _source = new CachedResolver(
+                new MultiResolver(
+                    new ZipSource("specification.zip"),
+                    new DirectorySource("TestData")
+                ));
+        }
+
+        private static IResourceResolver _source;
+        public static IResourceResolver Source
+        {
+            get
+            {
+                return _source;
+            }
+        }
+
+        public static int DumpTree(StructureItem tree)
+        {
+            int count = 1;
+            if (!string.IsNullOrEmpty(tree.SlicedPath))
+                System.Diagnostics.Debug.WriteLine($"{tree.Path}\t\t{tree.FixedValuesInSlices?.Count}\t\t{tree.MapTo("tcm")}");
+            else
+                System.Diagnostics.Debug.WriteLine($"{tree.Path}\t\t-->{tree.SlicedPath}\t{tree.FixedValuesInSlices?.Count}\t{tree.MapTo("tcm")}");
+            foreach (var item in tree.Children)
+            {
+                count += DumpTree(item);
+            }
+            return count;
+        }
+    }
+}
---
+++ src/Hl7.Fhir.Questionnaire/ValidationAlternative.cs
@@ -0,0 +1,52 @@
+﻿using Hl7.Fhir.ElementModel;
+using Hl7.Fhir.FhirPath;
+using Hl7.Fhir.Model;
+using System.Collections.Generic;
+
+namespace Hl7.Fhir.QuestionnaireServices
+{
+    public class ValidationProcessing
+    {
+        // Selection of the elements in the slice (create a fhirpath expression with a where to find all the children)
+        // these can be removed from the set, leaving all the other non sliced content
+
+        public OperationOutcome Validate(Base context, StructureDefinition sd)
+        {
+            StructureItem parent = StructureItemTree.CreateStructureTree(sd, null);
+            return parent.Validate(new PocoNavigator(context), new PocoNavigator(context));
+        }
+    }
+
+    /// <summary>
+    /// This is a subset of the ElementDefinition class with only enough
+    /// information for the validation system to work with
+    /// (and that we can quickly serialize to a binary form)
+    /// </summary>
+    public class MinimalElementDefinition
+    {
+        string id { get; set; }
+
+        /// <summary>
+        /// The value that is used for matching into the Questionnaire.(Group|Question).LinkId
+        /// </summary>
+        string code { get; set; }
+
+        /// <summary>
+        /// The label that can be used to display in error messages
+        /// </summary>
+        string shortDescription { get; set; }
+        int minCardinality { get; set; }
+        long maxCardinality { get; set; }
+        List<Element> type { get; set; }
+        Element fixedValue { get; set; }
+        // pattern?
+        Element minValue { get; set; }
+        Element maxValue { get; set; }
+        long maxStringLength { get; set; }
+        // condition?
+        List<ElementDefinition.ConstraintComponent> constraints { get; set; }
+        string bindingStrenth { get; set; } // this is really the enumeration
+        string bindingReference { get; set; }
+        string bindingUri { get; set; }
+    }
+}
